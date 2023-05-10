0d4c473ef4281c1ad667910f07a077ce610be6e9
Pavel Ovchinnikov <opavel@internet.ru>
2023-05-10 10:56:38 +0300

    task: выполнены основные и доп.задания

--- main.py
+++ main.py
@@ -3,46 +3,43 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 app = flask.Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:123@localhost/test_db7'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Books.db'
 db = SQLAlchemy(app)
 
 
-class Message(db.Model):
+class Book(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    text = db.Column(db.String(512), nullable=False)
+    author = db.Column(db.String(512), nullable=False)
+    name = db.Column(db.String(512), nullable=False)
+    __table_args__ = (db.UniqueConstraint('author', 'name', name='_author_name_uc'),
+                     )
 
-    def __init__(self, text, tags):
-        self.text = text
-        self.tags = [
-            Tag(text=tag) for tag in tags.split(',')
-        ]
-
-
-class Tag(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    text = db.Column(db.String(32), nullable=False)
-
-    message_id = db.Column(db.Integer, db.ForeignKey('message.id'), nullable=False)
-    message = db.relationship('Message', backref=db.backref('tags', lazy=True))
+    def __init__(self, author, title):
+        self.author = author
+        self.name = title
 
 
 @app.route('/', methods=['GET'])
-def hello():
-    return flask.render_template('index.html', messages=Message.query.all())
+def index():
+    return flask.render_template('index.html', books=list(reversed(Book.query.all())))
 
 
-@app.route('/add_message', methods=['POST'])
-def add_message():
-    text = flask.request.form['text']
-    tag = flask.request.form['tag']
-    # messages.append(Message(text, tag))
-    db.session.add(Message(text, tag))
+@app.route('/add_book', methods=['POST'])
+def add_book():
+    author = flask.request.form['author']
+    title = flask.request.form['title']
+    db.session.add(Book(author, title))
     db.session.commit()
 
-    return flask.redirect(flask.url_for('hello'))
+    return flask.redirect(flask.url_for('index'))
 
 
-
+@app.route('/delete_all', methods=['GET'])
+def delete_all():
+    db.session.execute('DELETE FROM book')
+    db.session.commit()
+    
+    return flask.redirect(flask.url_for('index'))
 
 with app.app_context():
     db.create_all()
